73357957ce90f834fdfdbfd8c5fd771bd559c42f
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2022-03-31 13:56:11 +0000

    new categories

--- src/Components/CustomButton.js
+++ src/Components/CustomButton.js
@@ -2,9 +2,9 @@ import { Button } from '@chakra-ui/react';
 import React from 'react';
 import { Colors } from '../Constants/Colors';
 
-const Custombutton = ({ buttonText }) => {
+const Custombutton = ({ buttonText, onclick }) => {
     return (
-        <Button backgroundColor={Colors.primary} w={'100%'} height={14} color={Colors.white}>{buttonText}</Button>
+        <Button onClick={onclick} backgroundColor={Colors.primary} w={'100%'} height={14} color={Colors.white}>{buttonText}</Button>
     );
 }
 
