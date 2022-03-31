d66dd6b683e765a62a4f4a22372e0bbe31e96c49
Frank Kwabena Abrokwa <76174290+Kwabena-Abrokwa@users.noreply.github.com>
2022-03-31 18:31:04 +0000

    Forms

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Input } from "@chakra-ui/react";
+import React from "react";
+import { Colors } from "../Constants/Colors";
 
-const InputField = ({value, setValue}) => {
+const InputField = ({ value, setValue, placeholder }) => {
   return (
-    <input value={value} onChange={(e)=>setValue(e.target.value)}/>
-  )
-}
+    <Input
+      type="text"
+      width={"100%"}
+      variant={"flushed"}
+      borderColor={Colors.primary}
+      value={value}
+      onChange={setValue}
+      placeholder={placeholder}
+      my={4}
+    />
+  );
+};
 
-export default InputField
+export default InputField;
--- src/Pages/NewElectionPage.jsx
+++ src/Pages/NewElectionPage.jsx
@@ -8,40 +8,39 @@ import random from "../Assets/img/meal.png";
 import axios from "axios";
 import globals from "../globals";
 import Custombutton from "../Components/CustomButton";
+import InputField from "../Components/InputField";
+import { Colors } from "../Constants/Colors";
 
 const NewElectionPage = () => {
+  const [electionName, setElectionName] = useState("");
+  const [emailAddress, setemailAddress] = useState("");
+  const [slugName, setslugName] = useState("");
+  const [number, setnumber] = useState("");
+  const [rate, setrate] = useState("Ghc" + "");
 
-
-  const [electionName, setElectionName] = useState("")
-  const [emailAddress, setemailAddress] = useState("")
-  const [slugName, setslugName] = useState("")
-  const [number, setnumber] = useState("")
-  const [rate, setrate] = useState("Ghc" + "")
-
-
-
-  const [loading, setloading] = useState(false)
-  const [data, setdata] = useState([])
-  const [categoriesData, setCategoriesData] = useState([])
+  const [loading, setloading] = useState(false);
+  const [data, setdata] = useState([]);
+  const [categoriesData, setCategoriesData] = useState([]);
   let { election } = useParams();
-  console.log(election)
+  console.log(election);
 
   const createElection = () => {
-    console.log("Initial Request")
-    axios.post(`${globals.baseUrl}/elections`,{
-      name:electionName,
-      email:emailAddress,
-      number:number,
-      slugName:slugName,
-      emoji:"‼️"
-    })
-    .then(() =>{
-      console.log("Success")
-    })
-    .catch(()=>{
-      console.log("Fail")
-    })
-  }
+    console.log("Initial Request");
+    axios
+      .post(`${globals.baseUrl}/elections`, {
+        name: electionName,
+        email: emailAddress,
+        number: number,
+        slugName: slugName,
+        emoji: "‼️",
+      })
+      .then(() => {
+        console.log("Success");
+      })
+      .catch(() => {
+        console.log("Fail");
+      });
+  };
 
   // useEffect(() => {
   //   const mounted = true;
@@ -60,46 +59,84 @@ const NewElectionPage = () => {
   //   return () => {};
   // }, []);
 
-  return (
-    loading 
-    ? 
-    <Box d="flex" w="100%" h="100vh" justifyContent="space-between" alignItems="center">
-      <Spinner size='xl' mx="auto" color='red.500'/>
+  return loading ? (
+    <Box
+      d="flex"
+      w="100%"
+      h="100vh"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Spinner size="xl" mx="auto" color="red.500" />
     </Box>
-    :
-      <>
+  ) : (
+    <>
       <Container maxW="container.lg">
+        <Box w={"95%"} mx={"auto"}>
+          <Categoriesheader
+            currentRoute={"Create a new election"}
+            headerName="PrestoVotes🚩"
+            // subtitle={`${categoriesData.length} entries`}
+          />
 
+          <InputField
+            placeholder={"Name of Election"}
+            value={slugName}
+            setValue={(e) => setslugName(e.target.value)}
+          />
 
+          <InputField
+            placeholder={"Nickname"}
+            value={slugName}
+            setValue={(e) => setslugName(e.target.value)}
+          />
 
-    <Box w={"95%"} mx={"auto"}>
-      <Categoriesheader
-        currentRoute={"Create a new election"}
-        headerName="PrestoVotes🚩"
-        // subtitle={`${categoriesData.length} entries`}
-      />
+          <InputField
+            placeholder={"Email Address"}
+            value={slugName}
+            setValue={(e) => setslugName(e.target.value)}
+          />
+          <h6 color={Colors.ash}>
+            This should be less that 10 characters with no spacing
+          </h6>
 
-      <input type="text" className="inputField" placeholder="Name Of Election"  value={electionName} onChange={(e)=>setElectionName(e.target.value)} />
-      <input type="text" className="inputField" placeholder="nickname"  value={slugName} onChange={(e)=>setslugName(e.target.value)} />
-      <h6 color="grey">this should be less that 10 characters with no spacing</h6>
-      <input type="text" className="inputField" placeholder="Email Address"  value={emailAddress} onChange={(e)=>setemailAddress(e.target.value)} />
-      <input type="number" className="inputField" placeholder="Phone Number"  value={number} onChange={(e)=>setnumber(e.target.value)} />
-      <Box d="flex" >
-      <input type="text" className="inputField" disabled value="Ghc" style={{width:'15%', color:'darkgrey', marginRight:"5%"}}/>
-      <input type="number" className="inputField" placeholder="per vote"  value={rate} onChange={(e)=>setrate(e.target.value)} />
-      </Box>
-
-      
-    </Box>
-    <Box mx="auto" my="20" w="70%">
-          <Custombutton onclick={() => createElection()} buttonText={"Create a new election"} />
-    </Box>
-    </Container>
-
-
-
+          <InputField
+            placeholder={"Phone Number"}
+            value={slugName}
+            setValue={(e) => setslugName(e.target.value)}
+          />
+          <Box d="flex">
+            <Box style={{ width: "15%", marginRight: "5%" }}>
+              <InputField
+                placeholder={"Phone Number"}
+                value={"Ghc"}
+                setValue={(e) => setslugName(e.target.value)}
+              />
+            </Box>
+            <Box style={{ width: "65%", marginRight: "5%" }}>
+              <InputField
+                placeholder={"Amount"}
+                value={slugName}
+                setValue={(e) => setslugName(e.target.value)}
+              />
+            </Box>
+            <Box style={{ width: "30%", marginRight: "5%" }}>
+              <InputField
+                placeholder={"Phone Number"}
+                value={"Per vote"}
+                setValue={(e) => setslugName(e.target.value)}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Box mx="auto" my="20" w="70%">
+          <Custombutton
+            onclick={() => createElection()}
+            buttonText={"Create a new election"}
+          />
+        </Box>
+      </Container>
     </>
-
   );
 };
 
